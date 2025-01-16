d9cc9b8ecaf5445986fe7bb6f27ab0fb6e2d4091
Rechan Dinata <93799964+Zreechxnn@users.noreply.github.com>
2025-01-17 06:55:34 +0700

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -6,28 +6,33 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+
 struct Button {
     SDL_Rect rect;
     char label[10];
     SDL_Color color;
     SDL_Color textColor;
 };
+
 void drawText(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Color color, SDL_Rect* rect) {
     if (strlen(text) == 0) {
         printf("Text is empty, skipping rendering.\n");
         return;
     }
+
     SDL_Surface* surface = TTF_RenderText_Solid(font, text, color);
     if (!surface) {
         printf("TTF_RenderText_Solid Error: %s\n", TTF_GetError());
         return;
     }
+
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     if (!texture) {
         printf("SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
         SDL_FreeSurface(surface);
         return;
     }
+
     int text_width = surface->w;
     int text_height = surface->h;
     SDL_Rect textRect = {rect->x + (rect->w - text_width) / 2, rect->y + (rect->h - text_height) / 2, text_width, text_height};
@@ -36,6 +41,7 @@ void drawText(SDL_Renderer* renderer, TTF_Font* font, const char* text, SDL_Colo
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(surface);
 }
+
 double evaluateExpression(const char* expression) {
     double result = 0;
     char op = 0;
@@ -58,6 +64,7 @@ double evaluateExpression(const char* expression) {
     }
     return result;
 }
+
 int main(int argc, char* argv[]) {
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -69,6 +76,7 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return -1;
     }
+
     SDL_Window* window = SDL_CreateWindow("Calculator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 360, 460, SDL_WINDOW_SHOWN);
     if (!window) {
         printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
@@ -76,6 +84,7 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return -1;
     }
+
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
@@ -84,6 +93,7 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return -1;
     }
+
     TTF_Font* font = TTF_OpenFont("lonely.ttf", 24);
     if (!font) {
         printf("Failed to load font: %s\n", TTF_GetError());
@@ -93,6 +103,7 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return -1;
     }
+
     struct Button buttons[20] = {
         {{10, 110, 80, 50}, "7", {0, 128, 255, 255}, {255, 255, 255, 255}},
         {{100, 110, 80, 50}, "8", {0, 128, 255, 255}, {255, 255, 255, 255}},
@@ -113,6 +124,7 @@ int main(int argc, char* argv[]) {
         {{10, 350, 170, 50}, "=", {0, 255, 0, 255}, {255, 255, 255, 255}},
         {{190, 350, 170, 50}, "DEL", {255, 0, 0, 255}, {255, 255, 255, 255}},
     };
+
     char currentExpression[100] = "";
     SDL_Color displayColor = {255, 255, 255, 255};
     SDL_Rect displayRect = {10, 10, 340, 90};
@@ -148,6 +160,7 @@ int main(int argc, char* argv[]) {
                 }
             }
         }
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 128, 255); // Dark blue background
         SDL_RenderClear(renderer);
         
